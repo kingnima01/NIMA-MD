978413f41dd49bca9677aece883dd99ea219ba31
kingnima01 <nimnadulshan80@gmail.com>
2024-11-16 08:39:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "ZKE1HaLY#dOJwNyVk5hlavQ7k6DbrWIFPzIPaPNDo4gB_BDWq5ow",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
